dad4e1908e6aa640f457f22ec1780847e83c5569
Nicolas Berenguela <nicolas.berenguela@gmail.com>
2024-06-20 01:39:56 -0400

    Avanzando de a poco

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,31 @@
 const express = require('express')
 
-const router = express.router();
+const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.json({mssg: "GET ALL LOGINS"})
+})
+
+
+//Obtener 1 login al peo
+
+router.get('/:id', (req,res) => {
+    res.json({mssg: "Obtenemos los datos del ql"})
+})
+
+// Añadir a un pacientes
+router.post('/', (req, res) => {
+    res.json({mssg: "ANADIR A ALGUIEN A LA BD"})
+})
+
+// Borrar un paciente
+router.delete('/', (req,res) => {
+    res.json({mssg: "Paciente eliminado"})
+})
+
+// Actualizar a un paciente
+router.patch('/', (req, res) => {
+    res.json({mssg: "Paciente actualizado"})
+})
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,38 @@
 const express = require('express')
 require('dotenv').config()
 
+// Conexion a la base de datos 
+const mongoose = require('mongoose')
+
+
+const loginRoutes = require('./routes/workouts')
+
 
 // Crea la app express
 const app = express()
 
-//MiddleWare
+// MiddleWare
+app.use(express.json())
+
+
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
 })
 
 
-// Rutas de la API
-app.get('/', (req, res) => {
-    res.json({mssg: "Bienvenido a la App"});
-})
+app.use('/api/workouts',loginRoutes)
+
+// Conectarse a la BD 
+mongoose.connect(process.env.MONGO_URI)
+    .then(( ) => {
+        app.listen(process.env.PORT, () => {
+            console.log("Escuchando en el puerto 4000");
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 
 // Escuchando llamadas
-app.listen(process.env.PORT, () => {
-    console.log("Escuchando en el puerto 4000");
-})
 
-process.env
